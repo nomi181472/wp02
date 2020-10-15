3ad252d9ebba5bc23d434ff1635f095f36fabda3
nomi181472 <nomansoomro52@gmail.com>
2020-10-15 21:21:43 +0500

    first comment

---
+++ script/index.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("afterNav").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("afterNav").style.marginLeft= "0";
+  }
